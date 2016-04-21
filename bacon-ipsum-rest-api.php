c5df7d7c2985bae6daf6bfcec7749f5f7994264c
pete@petenelson.com <petenelson@fortress.local>
2016-04-21 18:41:58 -0500

    initial commit

---
+++ bacon-ipsum-rest-api.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Bacon-ipsum-rest-api
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: bacon-ipsum-rest-api
+ * Domain Path: /languages
+ * @package Bacon-ipsum-rest-api
+ */
