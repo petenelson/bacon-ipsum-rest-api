430089c287eeb123fbba30e8dd44ea2531b6cad8
pete@petenelson.com <petenelson@fortress.local>
2016-04-21 19:30:23 -0500

    implemented REST API to generate filler

--- bacon-ipsum-rest-api.php
+++ bacon-ipsum-rest-api.php
@@ -1,12 +1,22 @@
 <?php
 /**
- * Plugin Name: Bacon-ipsum-rest-api
- * Version: 0.1-alpha
- * Description: PLUGIN DESCRIPTION HERE
- * Author: YOUR NAME HERE
- * Author URI: YOUR SITE HERE
- * Plugin URI: PLUGIN SITE HERE
+ * Plugin Name: Bacon Ipsum REST API
+ * Version: 1.0.0
+ * Description: Endooint for baconipsum.com
+ * Author: Pete Nelson
+ * Author URI: https://baconipsum.com
+ * Plugin URI: https://baconipsum.com
  * Text Domain: bacon-ipsum-rest-api
  * Domain Path: /languages
- * @package Bacon-ipsum-rest-api
+ * @package bacon-ipsum-rest-api
  */
+
+if ( ! defined( 'BACON_IPSUM_REST_API_ROOT' ) ) {
+	define( 'BACON_IPSUM_REST_API_ROOT', trailingslashit( dirname( __FILE__ ) ) );
+}
+
+require_once BACON_IPSUM_REST_API_ROOT . 'includes/class-bacon-ipsum-rest-api.php';
+
+add_action( 'plugins_loaded', function() {
+	Bacon_Ipsum_REST_API::plugins_loaded();
+} );
---
+++ includes/class-bacon-ipsum-rest-api.php
@@ -0,0 +1,63 @@
+<?php
+
+class Bacon_Ipsum_REST_API {
+
+	static public function plugins_loaded() {
+		add_action( 'rest_api_init', array( __CLASS__, 'register_endpoint' ) );
+
+		add_filter ( 'rest_pre_serve_request', array( __CLASS__, 'maybe_return_text' ), 10, 4 );
+
+	}
+
+	static public function register_endpoint() {
+
+		$args = array(
+			'number-of-paragraphs' => array(
+				'default'   => 1,
+				'sanitize_callback' => 'absint',
+				),
+			'format' => array(
+				'sanitize_callback' => 'sanitize_key',
+				'default' => 'json',
+				),
+			);
+
+		register_rest_route(
+			'baconipsum',
+			"/v1/generate",
+			array(
+				'methods'    => WP_REST_Server::READABLE,
+				'callback'   => array( __CLASS__, 'generate_filler' ),
+				'args'       => $args,
+				)
+			);
+
+	}
+
+	static public function generate_filler( $request ) {
+
+		$response = new stdClass();
+
+		$response->filler = apply_filters( 'anyipsum-generate-filler', array(
+			'number-of-paragraphs'   => $request['number-of-paragraphs'],
+			)
+		);
+
+		return rest_ensure_response( $response );
+	}
+
+
+	static public function validate_slack_token( $token ) {
+		return $token === get_option( 'bacon-ipsum-rest-api-slack-token' );
+	}
+
+	static public function maybe_return_text( $served, $response, $request, $server ) {
+		if ( 'text' === $request['format'] ) {
+			header( 'Content-Type: text/plain' );
+			echo implode( "\n\n", $response->data->filler );
+			exit;
+		}
+	}
+
+
+}
